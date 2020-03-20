1a88fef3735cd01d250605504041eb19b93d11b4
Magdalena Sremska <magdalena.sremska@gmail.com>
2020-03-20 23:59:05 +0100

    Optimisation

--- 5-Form-v1/app.js
+++ 5-Form-v1/app.js
@@ -6,50 +6,37 @@ class Form extends React.Component {
     number: "2",
   }
 
-  handleCityChange = e => {
-    this.setState({
-      city: e.target.value,
+  handleChange = e => {
+    if(e.target.type === "checkbox") {
+      this.setState({
+        [e.target.name]: e.target.checked
+      })
+    } else {
+      this.setState({
+        [e.target.name]: e.target.value
     })
   }
-
-  handleTextChange = e => {
-    this.setState({
-      text: e.target.value,
-    })
   }
-
-  handleIsLovedChange = e => {
-    this.setState({
-      isLoved: e.target.checked,
-    })
-  }
-
-  handleVisitsNumberChange(e) {
-    this.setState({
-      number: event.target.value,
-    })
-  }
-
   render() {
     return (
       <div>
         <label>
           Podaj miasto
-          <input value={this.state.city} onChange={this.handleCityChange} type="text" />
+          <input name="city" value={this.state.city} onChange={this.handleChange} type="text" />
         </label>
         <br />
         <label>Napisz coś o tym mieście
-          <textarea value={this.state.text} onChange={this.handleTextChange}></textarea>
+          <textarea name="text" value={this.state.text} onChange={this.handleChange}></textarea>
         </label>
         <br />
         <label>
           Czy lubisz to miasto?
-          <input type="checkbox" checked={this.state.isLoved} onChange={this.handleIsLovedChange} />
+          <input name="isLoved" type="checkbox" checked={this.state.isLoved} onChange={this.handleChange} />
         </label>
         <br />
         <label>
           Ile razy byliście w tym mieście?
-          <select value={this.state.number} onChange={this.handleVisitsNumberChange.bind(this)}>
+          <select name="number" value={this.state.number} onChange={this.handleChange}>
             <option value="0">0</option>
             <option value="1">1</option>
             <option value="2">2</option>
@@ -58,7 +45,6 @@ class Form extends React.Component {
           </select>
         </label>
       </div>
-
     )
   }
 }
