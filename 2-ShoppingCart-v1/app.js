9df4de51a9f4b79a2ca6998b931ab274e41994d5
Magdalena Sremska <magdalena.sremska@gmail.com>
2020-03-20 18:12:26 +0100

    ShoppingCart, structure & logic, styles, destructuring

---
+++ 2-ShoppingCart-v1/app.js
@@ -0,0 +1,41 @@
+class App extends React.Component {
+  state = {
+    availableProducts: 10,
+    shoppingCart: 0,
+  }
+
+handleRemoveFromCart = () => {
+  this.setState({
+    shoppingCart: this.state.shoppingCart - 1
+  })
+}
+
+handleAddToCart = () => {
+  this.setState({
+    shoppingCart: this.state.shoppingCart + 1
+  })
+}
+
+handleBuy = () => {
+  this.setState({
+    availableProducts: this.state.availableProducts - this.state.shoppingCart,
+    shoppingCart: 0
+  })
+}
+
+  render() {
+    const {shoppingCart, availableProducts} = this.state;
+    const style = shoppingCart === 0 ? {opacity: 0.3} : {};
+    
+    return (
+      <>
+        <button disabled={shoppingCart === 0 ? true : false} onClick={this.handleRemoveFromCart}>-</button>
+        <span style={style}> {shoppingCart} </span>
+        <button disabled={shoppingCart === availableProducts ? true : false} onClick={this.handleAddToCart}>+</button>
+        {shoppingCart > 0 && <button onClick={this.handleBuy}>Kup</button>}
+      </>
+    )
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
