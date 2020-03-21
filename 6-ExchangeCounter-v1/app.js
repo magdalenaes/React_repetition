ed44741d2040b299c9b52ba6f8037b3d7aa98d16
Magdalena Sremska <magdalena.sremska@gmail.com>
2020-03-21 01:08:25 +0100

    add select

--- 6-ExchangeCounter-v1/app.js
+++ 6-ExchangeCounter-v1/app.js
@@ -1,5 +1,5 @@
 const Cash = (props) => {
-  const value = (props.cash / props.ratio).toFixed(2)
+  const value = (props.cash / props.ratio * props.price).toFixed(2)
   return (
     <div>{props.title} {props.cash <= 0 ? "" : value}</div>
   )
@@ -12,26 +12,39 @@ class ExchangeCounter extends React.Component {
     product: "gas",
   }
 
-  currencies = [
-    {
-      id: 1,
-      name: "dollar",
-      ratio: 3.6,
-      title: "Wartość w dolarach:"
-    },
-    {
-      id: 2,
-      name: "euro",
-      ratio: 4.1,
-      title: "Wartość w euro:"
-    },
-    {
-      id: 3,
-      name: "pound",
-      ratio: 4.55,
-      title: "Wartość w funtach:"
-    },
-  ]
+  static defaultProps = {
+    currencies: [
+      {
+        id: 0,
+        name: "zloty",
+        ratio: 1,
+        title: "Wartość w złotówkach:"
+      },
+      {
+        id: 1,
+        name: "dollar",
+        ratio: 3.6,
+        title: "Wartość w dolarach:"
+      },
+      {
+        id: 2,
+        name: "euro",
+        ratio: 4.1,
+        title: "Wartość w euro:"
+      },
+      {
+        id: 3,
+        name: "pound",
+        ratio: 4.55,
+        title: "Wartość w funtach:"
+      },
+    ],
+    prices: {
+      electricity: .51,
+      gas: 4.76,
+      oranges: 3.79,
+    }
+  }
 
   handleChange = e => {
     this.setState({
@@ -41,7 +54,8 @@ class ExchangeCounter extends React.Component {
 
   handleSelect = e => {
     this.setState({
-      product: e.target.value
+      product: e.target.value,
+      amount: ""
     })
   }
 
@@ -52,16 +66,25 @@ class ExchangeCounter extends React.Component {
     else return null
   }
 
+  selectPrice(select) {
+    const price = this.props.prices[select]
+    return price
+  }
+
   render() {
 
     const { amount, product } = this.state;
-    const calculators = this.currencies.map(currency => (
+    const price = this.selectPrice(product)
+
+    const calculators = this.props.currencies.map(currency => (
       <Cash 
       key={currency.id} 
       ratio={currency.ratio} 
       title={currency.title} 
-      cash={amount}/>
+      cash={amount}
+      price={price}/>
     ))
+
     return (
       <div className="app">
         <label>Wybierz produkt:
